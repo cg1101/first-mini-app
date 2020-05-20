3b1e89d2cc4ecc7d346132a1f2b99b5bc4211c29
Gang Cheng <gang.cheng@ir.com>
2020-05-20 22:24:35 +1000

    add more example

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -1,15 +1,35 @@
 Page({
   data: {
+    states: [
+      { label: 'New South Wales', value: 'NSW' },
+      { label: 'Australia Capital Teritory', value: 'ACT', disabled: true },
+      { label: 'Victoria', value: 'VCT', checked: true },
+      { label: 'Tasmania', value: 'TAS' },
+      { label: 'West Australia', value: 'WA' },
+    ],
     form0: {
       name: '',
       registered: false,
     },
     form1: {
-
+      street1: '',
+      street2: '',
+      city: '',
+      state: '',
+      country: '',
+      zip: '',
     },
   },
   onLoad() {
 
+  },
+  statesUpdated(e: any) {
+    console.log('statesUpdated', e);
+    console.log('data.states ->', this.data.states);
+  },
+  handleContact (e: any) {
+    console.log(e.detail.path)
+    console.log(e.detail.query)
   },
   submitForm(e: any) {
     console.log('submitForm()', e);
