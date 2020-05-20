fdb66df52b98ca4498bc22d37a6898c78b7b0ada
Gang Cheng <gang.cheng@ir.com>
2020-05-20 20:46:57 +1000

    add example for forms

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -1,9 +1,26 @@
 Page({
   data: {
+    form0: {
+      name: '',
+      registered: false,
+    },
+    form1: {
 
+    },
   },
   onLoad() {
 
   },
-  
+  submitForm(e: any) {
+    console.log('submitForm()', e);
+    const formId = e.target.id;
+    const eventType = e.type;
+    console.log(`form #${formId}: ${eventType}`);
+  },
+  resetForm(e: any) {
+    console.log('resetForm()', e);
+    const formId = e.target.id;
+    const eventType = e.type;
+    console.log(`form #${formId}: ${eventType}`);
+  }
 })
