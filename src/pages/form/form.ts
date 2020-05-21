ce4f3e6321414debc5e98f37204874fbf1dd2117
Gang Cheng <gang.cheng@ir.com>
2020-05-21 13:57:52 +1000

    improve form demo

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -1,3 +1,5 @@
+let timerId: number = NaN;
+
 Page({
   data: {
     states: [
@@ -19,9 +21,69 @@ Page({
       country: '',
       zip: '',
     },
+    sliderDemo: {
+      disabled: false,
+      activeColor: 'blue',
+      backgroundColor: 'darkgray',
+      blockColor: 'lightgray',
+      blockSize: '12',
+      showValue: true,
+    },
+    iconDemo: {
+      types: ['success', 'success_no_circle', 'info', 'warn',
+        'waiting', 'cancel', 'download', 'search', 'clear'],
+      typeIndex: 0,
+      size: 23,
+      color: 'green',
+    },
+    timer: {
+      running: false,
+      label: 'Start Timer',
+    },
+    progressDemo: {
+      percent: 50,
+      showInfo: true,
+      radius: 2,
+      fontSize: 16,
+      strokeWidth: 2,
+      active: false, // do not show progressing animation
+      activeMode: 'backwards',
+      isBackwards: true,
+      duration: 30,
+      activeColor: 'purple', // '#09BB07',
+      backgroundColor: 'yellow', //'#EBEBEB',
+    },
   },
   onLoad() {
 
+  },
+  onHide() {
+    this.stopTimer();
+  },
+  startTimer() {
+    timerId = setInterval(this.reportProgress.bind(this), 200);
+    const timer = { ...this.data.timer, running: true, label: 'Stop Timer' };
+    this.setData({ timer});
+  },
+  stopTimer() {
+    if (timerId) {
+      clearInterval(timerId);
+      timerId = NaN;
+      const timer = { ...this.data.timer, running: false, label: 'Start Timer' };
+      this.setData({ timer});
+    }
+  },
+  reportProgress() {
+    const progressDemo = { ...this.data.progressDemo };
+    let completion = progressDemo.percent;
+    if (completion < 100) {
+      completion += 1;
+      progressDemo.percent = completion;
+      this.setData({ progressDemo });
+    }
+    if (completion >= 100) {
+      this.stopTimer();
+    }
   },
   statesUpdated(e: any) {
     console.log('statesUpdated', e);
@@ -42,5 +104,26 @@ Page({
     const formId = e.target.id;
     const eventType = e.type;
     console.log(`form #${formId}: ${eventType}`);
-  }
+  },
+  changeIconType(e: any) {
+    const typeIndex = e.detail.value;
+    console.log(`Setting typeIndex to ${typeIndex}`);
+    const iconDemo = { ...this.data.iconDemo, typeIndex };
+    this.setData({ iconDemo });
+  },
+  activeEnd(e: any) {
+    console.log('progress reported ends', e);
+  },
+  toggleTimer(e: any) {
+    console.log('toggleTimer triggered', e);
+    if (this.data.timer.running) {
+      this.stopTimer();
+    } else {
+      this.startTimer();
+    }
+  },
+  resetProgress() {
+    const progressDemo = { ...this.data.progressDemo, percent: 0 };
+    this.setData({ progressDemo });
+  },
 })
