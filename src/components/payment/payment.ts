6b2c943a0ba9388aabac5356e2b7b0781344e5e7
Gang Cheng <gang.cheng@ir.com>
2020-05-25 08:12:10 +1000

    fix bug in payment component

--- src/components/payment/payment.ts
+++ src/components/payment/payment.ts
@@ -14,37 +14,27 @@ Component({
     expiryMonth: '',
     expiryYear: '',
     cvv: '',
+    showCardInfo: false,
     $valid: false,
   },
   observers: {
     // here we must use function expression instead of arrow function
     // because of arrow function's lack of bindings for `this`
-    'cardNumber, expiryMonth, expiryYear, cvv': function (
-      cardNumberEvent: any,
-      expiryMonthEvent: any,
-      expiryYearEvent: any,
-      cvvEvent: any,
+    'cardNumber, expiryMonth, expiryYear, cvv': function cardInfoObserver(
+      cardNumber: string, expiryMonth: string, expiryYear: string, cvv: string,
     ) {
-      const { value: cardNumber } = cardNumberEvent;
-      const { value: expiryMonth } = expiryMonthEvent;
-      const { value: expiryYear } = expiryYearEvent;
-      const { value: cvv } = cvvEvent;
-
-      const $valid = this.validateCreditCardInfo({
-        cardNumber, expiryMonth, expiryYear, cvv
-      });
+      const $valid = this.validateCreditCardInfo(cardNumber, expiryMonth, expiryYear, cvv);
       this.setData({ $valid });
     }
   },
   methods: {
-    validateCreditCardInfo(cardInfo: {
+    validateCreditCardInfo(
       cardNumber: string,
       expiryMonth: string,
       expiryYear: string,
       cvv: string,
-    }) {
+    ) {
       console.log('validate credit card info');
-      const { cardNumber, expiryMonth, expiryYear, cvv } = cardInfo;
       const cardType = CreditCard.getCardType(cardNumber);
       const isCvvOkay = /^\d{3,}$/.test(cvv);
       return CreditCard.luhnCheck(cardNumber) &&
@@ -54,39 +44,52 @@ Component({
     },
     usePayTypeWeChat(e: any) {
       console.log(`component payment tapped`, e);
-      const details = { payType: PayType.WECHAT, $valid: true}
+      const details = {
+        payType: PayType.WECHAT,
+        showCardInfo: false,
+        $valid: true,
+      };
       this.setData(details);
       this.triggerEvent('payTypeSelected', details, {});
     },
     usePayTypeBalance(e: any) {
       console.log(`component payment tapped`, e);
-      const details = { payType: PayType.BALANCE, $valid: true}
+      const details = {
+        payType: PayType.BALANCE,
+        showCardInfo: false,
+        $valid: true,
+      };
       this.setData(details);
       this.triggerEvent('payTypeSelected', details, {});
     },
     usePayTypeCreditCard(e: any) {
       console.log(`component payment tapped`, e);
-      const $valid = this.validateCreditCardInfo(this.data);
-      this.setData({ payType: PayType.CREDIT_CARD, $valid });
+      const { cardNumber, expiryMonth, expiryYear, cvv } = this.data;
+      const $valid = this.validateCreditCardInfo(cardNumber, expiryMonth, expiryYear, cvv);
+      this.setData({
+        payType: PayType.CREDIT_CARD,
+        showCardInfo: true,
+        $valid,
+      });
     },
     updateCardNumber(e: any) {
-      console.log('updateCardNumber()', e);
-      const { detail: cardNumber } = e;
+      // console.log('updateCardNumber()', e);
+      const { value: cardNumber } = e.detail;
       this.setData({ cardNumber });
     },
     updateExpiryMonth(e: any) {
-      console.log('updateExpiryMonth()', e);
-      const { detail: expiryMonth } = e;
+      // console.log('updateExpiryMonth()', e);
+      const { value: expiryMonth } = e.detail;
       this.setData({ expiryMonth });
     },
     updateExpiryYear(e: any) {
-      console.log('updateExpiryYear()', e);
-      const { detail: expiryYear } = e;
+      // console.log('updateExpiryYear()', e);
+      const { value: expiryYear } = e.detail;
       this.setData({ expiryYear });
     },
     updateCvv(e: any) {
-      console.log('updateCvv()', e);
-      const { detail: cvv } = e;
+      // console.log('updateCvv()', e);
+      const { value: cvv } = e.detail;
       this.setData({ cvv });
     },
   },
