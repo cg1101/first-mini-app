9286389ff9775aea2d60948b6471fdd589ce022f
Gang Cheng <gang.cheng@ir.com>
2020-05-30 16:25:21 +1000

    add picker example

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -9,6 +9,29 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     showTabBar: true,
+    multiArray: [
+      ['无脊柱动物', '脊柱动物'],
+      ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'],
+      ['猪肉绦虫', '吸血虫'],
+    ],
+    objectMultiArray: [
+      [
+        { id: 0, name: '无脊柱动物' },
+        { id: 1, name: '脊柱动物' }
+      ],
+      [
+        { id: 0, name: '扁性动物' },
+        { id: 1, name: '线形动物' },
+        { id: 2, name: '环节动物' },
+        { id: 3, name: '软体动物' },
+        { id: 3, name: '节肢动物' }
+      ],
+      [
+        { id: 0, name: '猪肉绦虫' },
+        { id: 1, name: '吸血虫' }
+      ]
+    ],
+    multiIndex: [0, 0, 0],
   },
   // 事件处理函数
   bindViewTap() {
@@ -74,5 +97,74 @@ Page({
     wx.navigateTo({
       url: '/pages/checkout/checkout?order_type=buyNow&goods_id=10002&goods_num=1&goods_sku_id=0',
     });
+  },
+  bindMultiPickerChange(e: any) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      multiIndex: e.detail.value
+    })
+  },
+  bindMultiPickerColumnChange(e: any) {
+    console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
+    var data = {
+      multiArray: this.data.multiArray,
+      multiIndex: this.data.multiIndex
+    };
+    data.multiIndex[e.detail.column] = e.detail.value;
+    switch (e.detail.column) {
+      case 0:
+        switch (data.multiIndex[0]) {
+          case 0:
+            data.multiArray[1] = ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'];
+            data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
+            break;
+          case 1:
+            data.multiArray[1] = ['鱼', '两栖动物', '爬行动物'];
+            data.multiArray[2] = ['鲫鱼', '带鱼'];
+            break;
+        }
+        data.multiIndex[1] = 0;
+        data.multiIndex[2] = 0;
+        break;
+      case 1:
+        switch (data.multiIndex[0]) {
+          case 0:
+            switch (data.multiIndex[1]) {
+              case 0:
+                data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
+                break;
+              case 1:
+                data.multiArray[2] = ['蛔虫'];
+                break;
+              case 2:
+                data.multiArray[2] = ['蚂蚁', '蚂蟥'];
+                break;
+              case 3:
+                data.multiArray[2] = ['河蚌', '蜗牛', '蛞蝓'];
+                break;
+              case 4:
+                data.multiArray[2] = ['昆虫', '甲壳动物', '蛛形动物', '多足动物'];
+                break;
+            }
+            break;
+          case 1:
+            switch (data.multiIndex[1]) {
+              case 0:
+                data.multiArray[2] = ['鲫鱼', '带鱼'];
+                break;
+              case 1:
+                data.multiArray[2] = ['青蛙', '娃娃鱼'];
+                break;
+              case 2:
+                data.multiArray[2] = ['蜥蜴', '龟', '壁虎'];
+                break;
+            }
+            break;
+        }
+        data.multiIndex[2] = 0;
+        break;
+    }
+    console.log(data.multiIndex);
+    this.setData(data);
   },
 })
