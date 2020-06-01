5c09f404dbc40f997fb2995dcb5c024ffb6fa6dc
Gang Cheng <gang.cheng@ir.com>
2020-06-01 13:22:46 +1000

    add suburbs module for oz localities

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,6 +1,16 @@
 // index.ts
 // 获取应用实例
+
+import { suburbs } from '../../utils/suburbs';
+
 const app = getApp<IAppOption>();
+
+const getSuburbs = (state: string, letter: string) => suburbs[state]
+  .filter((sub) => sub[0] === letter).sort();
+
+const states = Object.keys(suburbs).sort();
+const letters = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'.split(/\s/);
+const startsWith: string [] = getSuburbs(states[0], 'A');
 
 Page({
   data: {
@@ -10,26 +20,9 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     showTabBar: true,
     multiArray: [
-      ['无脊柱动物', '脊柱动物'],
-      ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'],
-      ['猪肉绦虫', '吸血虫'],
-    ],
-    objectMultiArray: [
-      [
-        { id: 0, name: '无脊柱动物' },
-        { id: 1, name: '脊柱动物' }
-      ],
-      [
-        { id: 0, name: '扁性动物' },
-        { id: 1, name: '线形动物' },
-        { id: 2, name: '环节动物' },
-        { id: 3, name: '软体动物' },
-        { id: 3, name: '节肢动物' }
-      ],
-      [
-        { id: 0, name: '猪肉绦虫' },
-        { id: 1, name: '吸血虫' }
-      ]
+      states,
+      letters,
+      startsWith,
     ],
     multiIndex: [0, 0, 0],
   },
@@ -106,65 +99,14 @@ Page({
   },
   bindMultiPickerColumnChange(e: any) {
     console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
-    var data = {
-      multiArray: this.data.multiArray,
-      multiIndex: this.data.multiIndex
-    };
-    data.multiIndex[e.detail.column] = e.detail.value;
-    switch (e.detail.column) {
-      case 0:
-        switch (data.multiIndex[0]) {
-          case 0:
-            data.multiArray[1] = ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'];
-            data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
-            break;
-          case 1:
-            data.multiArray[1] = ['鱼', '两栖动物', '爬行动物'];
-            data.multiArray[2] = ['鲫鱼', '带鱼'];
-            break;
-        }
-        data.multiIndex[1] = 0;
-        data.multiIndex[2] = 0;
-        break;
-      case 1:
-        switch (data.multiIndex[0]) {
-          case 0:
-            switch (data.multiIndex[1]) {
-              case 0:
-                data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
-                break;
-              case 1:
-                data.multiArray[2] = ['蛔虫'];
-                break;
-              case 2:
-                data.multiArray[2] = ['蚂蚁', '蚂蟥'];
-                break;
-              case 3:
-                data.multiArray[2] = ['河蚌', '蜗牛', '蛞蝓'];
-                break;
-              case 4:
-                data.multiArray[2] = ['昆虫', '甲壳动物', '蛛形动物', '多足动物'];
-                break;
-            }
-            break;
-          case 1:
-            switch (data.multiIndex[1]) {
-              case 0:
-                data.multiArray[2] = ['鲫鱼', '带鱼'];
-                break;
-              case 1:
-                data.multiArray[2] = ['青蛙', '娃娃鱼'];
-                break;
-              case 2:
-                data.multiArray[2] = ['蜥蜴', '龟', '壁虎'];
-                break;
-            }
-            break;
-        }
-        data.multiIndex[2] = 0;
-        break;
+    const { column, value } = e.detail;
+    const { multiArray, multiIndex } = this.data;
+    multiIndex[column] = value;
+    const state = states[multiIndex[0]];
+    const letter = letters[multiIndex[1]];
+    if (column < 2) {
+      multiArray[2] = getSuburbs(state, letter);
     }
-    console.log(data.multiIndex);
-    this.setData(data);
+    this.setData({ multiArray, multiIndex });
   },
 })
